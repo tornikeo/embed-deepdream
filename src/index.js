d3bb7968fac88bd81a40cdb48054a8443feae6b7
tornikeo <tornikeonoprishvili@gmail.com>
2022-06-17 00:10:18 +0400

    Restart everything from webpack-boilerplate code

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import { example } from '@/js/example'
 // Test import of an asset
 import webpackLogo from '@/images/webpack-logo.svg'
 
+import startImage from '@/images/monkey.png'
+
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
 const logo = document.createElement('img')
-logo.src = webpackLogo
+logo.src = startImage
 
 const heading = document.createElement('h1')
-heading.textContent = example()
+// heading.textContent = example()
+heading.textContent = "Deep Dream"
+
+const startButton = document.createElement('button');
+startButton.textContent = 'Start Deep Dream'
 
 // Test a background image url in CSS
 const imageBackground = document.createElement('div')
@@ -23,4 +29,4 @@ const imagePublic = document.createElement('img')
 imagePublic.src = '/assets/example.png'
 
 const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+app.append(logo, heading, startButton )
